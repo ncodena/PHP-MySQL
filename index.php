9952b0d43cb86ec536791d5369ed8f5b4abfa503
Nuria <c.odena.nuria@gmail.com>
2020-08-04 18:39:56 +0200

    constants & variables

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 
-echo 'hello world';
+define('NAME', 'Mario');
+
+// echo 'hello world';
+// $name = 'Yoshi';
+$age = 30;
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Document</title>
+</head>
+<body>
+<h1>User Profile Page</h1>
+<div><?php echo NAME;?> </div>
+<div><?php echo $age;?> </div>
+    
+</body>
+</html>
