e1fa76a2aaecbc9423f675d7bd1789aae36388e2
Nuria <c.odena.nuria@gmail.com>
2020-08-04 19:20:04 +0200

    numbers

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 
+// STRINGS
+
 // define('NAME', 'Mario');
 
 // echo 'hello world';
 // $name = 'Yoshi';
-$stringOne = 'my email is ';
-$stringTwo = 'xxxx@gmail.com ';
+// $stringOne = 'my email is ';
+// $stringTwo = 'xxxx@gmail.com ';
 
 // echo $stringOne . $stringTwo;
 
-$name = 'mario';
+// $name = 'mario';
 
 // echo "Hey, my name is $name";
 
@@ -20,9 +22,44 @@ $name = 'mario';
 // echo strlen($name);
 // echo strtoupper($name);
 // echo strtolower($name);
-echo str_replace('m', 'w', $name);
+// echo str_replace('m', 'w', $name);
+
+// NUMBERS
+
+$radius = 25;
+$pi = 3.14;
 
 
+
+// basic - *, /, +, -, **
+
+// echo $pi * $radius**2;
+
+// order of operation: (Brackets, Index, Division, Multiplication, Addition, Substraction )
+
+// echo 2 * (4 + 9) / 3;
+
+
+
+// increment & decrement operators
+
+// echo $radius++;
+// echo $radius;
+
+
+
+// shorthand operators
+
+// $age = 20;
+// $age -=10;
+// echo $age;
+
+
+// number functions
+
+// echo floor($pi);
+// echo ceil($pi);
+// echo pi();
 ?>
 
 <!DOCTYPE html>
