4a743b733f0fe59658b845fa89caedc78fcde035
Nuria <c.odena.nuria@gmail.com>
2020-08-06 11:12:43 +0200

    variable scope

--- index.php
+++ index.php
@@ -250,6 +250,44 @@
 // echo $formatted;
 
 
+// VARIABLE SCOPE
+
+// Local variables
+
+// function myFunc(){
+//     $price = 10;
+//     echo $price;
+// }
+
+// myFunc();
+
+// function myFuncTwo($age){
+//     echo $age;
+// }
+
+// myFuncTwo(25);
+
+// Global variables
+
+// $name = 'william';
+
+// function sayHello(){
+//     global $name;
+//     $name = 'mary';
+//     echo "hello $name";
+// }
+
+// sayHello();
+// echo $name;
+
+// function sayBye(&$name){
+//     $name = 'wario';
+//     echo "bye $name";
+// }
+
+// sayBye($name);
+// echo $name;
+
 // ?>
 
 <!DOCTYPE html>
