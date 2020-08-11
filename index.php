13ca27e6617303c30046f5ac864bc254bf8dd242
Nuria <c.odena.nuria@gmail.com>
2020-08-11 14:25:19 +0200

    superglobals

--- index.php
+++ index.php
@@ -294,6 +294,13 @@
 // require('group.php');
 // echo 'end of php';
 
+// SUPERGLOBALS
+
+echo $_SERVER['SERVER_NAME'] . '<br />';
+echo $_SERVER['REQUEST_METHOD'] . '<br />';
+echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+echo $_SERVER['PHP_SELF'] . '<br />';
+
 // ?>
 
 <!DOCTYPE html>
